0abf1e201fddb86ed180406e7b2ed191d140563f
MarioCares <lukos_mapyo@hotmail.com>
2013-02-02 14:03:28 -0300

    Agrego validación usuario simple

--- Controladores/Registro.php
+++ Controladores/Registro.php
@@ -1,3 +1,17 @@
 <?php
-	echo $_POST["usuario"]. " ". $_POST["pass"];
+/*
+*	SE ENCARGA DE VALIDAR EL LOGIN DE LOS USUARIOS
+*   VARIABLES: $_POST["usuario"] y $_POST["pass"]
+*/
+
+if (empty($_POST["usuario"]) || empty($_POST["pass"])) {
+    echo "Debe ingresar datos";
+}else{
+    if ($_POST["usuario"] == "inacap" && $_POST["pass"] == "inacap") {
+        # ADMINISTRADOR
+        $_SESSION["Usuario"] = "ADMINISTRADOR";
+    }else{
+        echo "mani :D ";
+    }
+}
 ?>
